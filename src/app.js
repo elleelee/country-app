a4fbcd5abac141ecd21303067fd937eaf80adf81
Ellen Lee <ellen811003@gmail.com>
2019-10-21 22:51:01 +0800

    prep for deployment

--- src/app.js
+++ src/app.js
@@ -9,19 +9,19 @@ app.use(express.json())
 app.use(userRouter)
 
 // console.log(process.env.NODE_ENV)
-// if(process.env.NODE_ENV === 'production') {
-//   app.use(express.static('client/build'));
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build/'));
 
-//   app.get('*', (req.res) => {
-//     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
+  app.get('*', (req.res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
-app.use(express.static(path.join(__dirname, "client", "build")))
+// app.use(express.static(path.join(__dirname, "client", "build")))
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
 
 
 
