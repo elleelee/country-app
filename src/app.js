ba138a1d5b8b7b3b7ddebbe81a15a03d9fe31dfd
Ellen Lee <ellen811003@gmail.com>
2019-10-21 22:13:11 +0800

    prep for deployment

--- src/app.js
+++ src/app.js
@@ -1,10 +1,28 @@
-const express = require('express')
-require('./db/mongoose')
-const userRouter = require('./routers/user')
+const express = require('express');
+require('./db/mongoose');
+const userRouter = require('./routers/user');
+const path = require('path');
 
-const app =express()
+const app =express();
 
 app.use(express.json())
 app.use(userRouter)
+
+// console.log(process.env.NODE_ENV)
+// if(process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
+
+//   app.get('*', (req.res) => {
+//     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
+
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+
 
 module.exports = app
