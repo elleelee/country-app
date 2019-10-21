22a924426d5dca49d2fed07bad80a4c19211e81b
Ellen Lee <ellen811003@gmail.com>
2019-10-21 23:28:28 +0800

    production routing

--- src/app.js
+++ src/app.js
@@ -10,11 +10,12 @@ app.use(userRouter)
 
 // console.log(process.env.NODE_ENV)
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, "client", "build")))
+  // app.use(express.static(path.join(__dirname, "client", "build")))
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  // });
+  app.use('*', express.static(path.join(__dirname, "client", "build")))
 }
 
 // app.use(express.static(path.join(__dirname, "client", "build")))
