24ce6a84f93716c111064903080cc884ba9a7638
Ellen Lee <ellen811003@gmail.com>
2019-10-22 08:31:26 +0800

    direct path set up

--- src/app.js
+++ src/app.js
@@ -10,14 +10,14 @@ app.use(userRouter)
 
 // console.log(process.env.NODE_ENV)
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'));
+  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
   });
 }
 
-// console.log(__dirname)
+// console.log(path.join(__dirname, '..', 'client', 'build', 'index.html'))
 
 // app.use(express.static(path.join(__dirname, "client", "build")))
 
